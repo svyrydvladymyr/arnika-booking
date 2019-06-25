b16a42d588462a2fc79f7104e35ece67e9e3113b
Свирид Володимир <38647042+svyrydvladymyr@users.noreply.github.com>
2019-06-25 16:13:59 +0300

    Add files via upload

---
+++ php/getForUpdateTwo.php
@@ -0,0 +1,22 @@
+<?php include 'access.php'?>
+
+<?php
+$obj = json_decode($_GET["x"], false);
+$objs = json_decode($_GET["x"], false);
+$conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare("SELECT login, password FROM users WHERE login = ? AND password = ?");
+$stmt->bind_param("ss", $obj->login, $obj->password);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+// echo json_encode($outp);
+if ($outp != []){
+    $stmts = $conn->prepare("SELECT last_name, first_name, nomer_kimn, telephone, kilk_dniv, data_zaizdu, price, admin, data_zapisu, tip, admin_updata, data_zmin, status FROM bookinggurt1 WHERE last_name = ? AND first_name = ? AND nomer_kimn = ? AND telephone = ? AND kilk_dniv = ? AND (status = 'pay' OR status = 'rezerv')");
+    $stmts->bind_param("sssss", $objs->lastname, $objs->firstname, $objs->nomerkimn, $objs->telephone, $objs->kilkdniv);
+    $stmts->execute();
+    $results = $stmts->get_result();
+    $outps = $results->fetch_all(MYSQLI_ASSOC);
+    echo json_encode($outps);
+}
+?>
+
---
+++ php/upToDBTwo.php
@@ -0,0 +1,26 @@
+<?php include 'access.php'?>
+
+<?php
+$obj = json_decode($_GET["x"], false);
+$objs = json_decode($_GET["x"], false);
+$conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare("SELECT login, password FROM users WHERE login = ? AND password = ?");
+$stmt->bind_param("ss", $obj->login, $obj->password);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+// echo json_encode($outp);
+if ($outp != []){
+    $stmts = $conn->prepare("UPDATE bookinggurt1 SET status = ?, admin_updata = ?, data_zmin = ? WHERE last_name = ? AND first_name = ? AND telephone = ? AND nomer_kimn = ? AND kilk_dniv = ? AND data_zapisu = ? AND (status = 'pay' OR status = 'rezerv')");
+    $stmts->bind_param("sssssssss", $objs->statusUp, $objs->adminregUp, $objs->dateregUp, $objs->surnameUp, $objs->nameUp, $objs->telUp, $objs->nomerUp, $objs->kilkUp, $objs->datazapisuUp);
+    $stmts->execute();
+    if ($stmts->error) {
+        echo "FAILURE!!! " . $stmts->error;
+    }
+    else echo "{$stmts->affected_rows}";  
+    $stmts->close();
+}
+?>
+
+
+
